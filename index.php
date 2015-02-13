924cd0aae365021d207a1530606d97eb89b35bd1
xiao <zghanxiao@gmail.com>
2015-02-12 16:34:34 -0800

    add point 2 dimensional data add json skeleton

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <title>
+    </title>
+    <body>
+
+    <script>
+        alert('hello js');
+    </script>
+
+<?php
+      $arr = array('a'=>1, 'b'=>2, 'c'=>3, 'd'=>4, 'e'=>5);
+      echo json_encode($arr);
+?>
+    </body>
+</html>
